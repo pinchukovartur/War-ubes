7ace5daf1c2a3486ebd5e2b6410f04b6ead29506
pinchukovartur <pinchukovartur@gmail.com>
2019-10-18 23:13:25 +0300

    first commit

---
+++ Assets/MoveForPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _player;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(_player.transform.position.x - 6, transform.position.y, _player.transform.position.z - 3);
+    }
+}
